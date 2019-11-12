a49ee37bbf44f57097740823c41b165dfae202ca
Dmitry Zlygin <dmitry.zlygin@nordigy.ru>
2019-11-12 19:24:22 +0300

    added permission tracking to reacquire devices list

--- src/components/MediaDeviceListProvider.tsx
+++ src/components/MediaDeviceListProvider.tsx
@@ -1,6 +1,29 @@
-import React, { FC, createContext, useEffect, useState } from 'react';
+import React, { FC, createContext, useEffect, useState, useReducer } from 'react';
 
 export const MediaDeviceListContext = createContext<MediaDeviceInfo[]>([]);
+
+type EmptyFn = () => void;
+
+const deviceRequestEffect = (deviceType: 'camera' | 'microphone', forceDeviceRequest: () => void): EmptyFn =>  {
+    let mediaPermissions: PermissionStatus | null = null;
+    navigator.permissions.query({ name: deviceType }).then(result => {
+        mediaPermissions = result;
+        if (result.state !== 'granted') {
+            result.onchange = () => {
+                if (result.state === 'granted') {
+                    forceDeviceRequest();
+                }
+            };
+        }
+    });
+
+    return () => {
+        if (mediaPermissions) {
+            mediaPermissions.onchange = null;
+            mediaPermissions = null;
+        }
+    };
+};
 
 const MediaDeviceListProvider: FC = ({ children }) => {
     const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
@@ -18,9 +41,20 @@ const MediaDeviceListProvider: FC = ({ children }) => {
         }
     }, []);
 
+    const [deviceRequestCounter, forceDeviceRequest] = useReducer<(s: number) => number>((state: number) => (state + 1), 0);
+
+    useEffect(() => {
+        if (deviceRequestCounter > 0) {
+            navigator.mediaDevices.enumerateDevices().then(setDevices);
+        }
+    }, [deviceRequestCounter]);
+
+    useEffect(() => deviceRequestEffect('camera', forceDeviceRequest as EmptyFn), []);
+    useEffect(() => deviceRequestEffect('microphone', forceDeviceRequest as EmptyFn), []);
+
     return <MediaDeviceListContext.Provider value={devices}>
         {children}
     </MediaDeviceListContext.Provider>;
-}
+};
 
 export default MediaDeviceListProvider;
